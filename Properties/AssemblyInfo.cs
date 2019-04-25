c2ca4f1b4cb738072aa997b6f12dbcb49889c956
alchemistmatt <matthew.monroe@pnnl.gov>
2019-04-24 18:51:44 -0700

    Convert to C#

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,32 +1,33 @@
-Imports System
-Imports System.Reflection
-Imports System.Runtime.InteropServices
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
-' General Information about an assembly is controlled through the following 
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DataFilter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataFilter")]
+[assembly: AssemblyCopyright("Copyright © PNNL 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
-' Review the values of the assembly attributes
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
 
-<Assembly: AssemblyTitle("")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("")> 
-<Assembly: AssemblyCopyright("")> 
-<Assembly: AssemblyTrademark("")> 
-<Assembly: CLSCompliant(True)> 
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("253dfde5-85c5-463d-aabb-49dcad93eab1")]
 
-'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("408B03F2-6FAA-4240-8028-F75519FBC677")> 
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.3.0.0")]
 
-' Version information for an assembly consists of the following four values:
-'
-'      Major Version
-'      Minor Version 
-'      Build Number
-'      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers 
-' by using the '*' as shown below:
-
-<Assembly: AssemblyVersion("1.2.*")> 
